1028fae0c1a58cee366f822102a76cfcfa864c60
Cha Yang <chachaiyang3@gmail.com>
2019-07-20 06:30:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,5 @@
 let timer = 10;
 
-function forLoop(number = []){
-  for(let i = 0; 1 < 25; i++){
-    number.push('I am a strange loop');
-  }
-}
-
 function whileLoop(){
   while(timer > 0){
     console.log(--timer);
